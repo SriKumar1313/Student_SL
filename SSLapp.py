cc6396477aedaf9b8061f6f3642a0d8a29e473e4
SriKumar1313 <149764826+SriKumar1313@users.noreply.github.com>
2024-06-24 23:12:51 +0530

    Update SSLapp.py

--- SSLapp.py
+++ SSLapp.py
@@ -3,14 +3,12 @@ import pickle
 import numpy as np
 import pandas as pd
 
-# Load the trained model and preprocessor
 with open('best_decision_tree_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 with open('preprocessor.pkl', 'rb') as file:
     preprocessor = pickle.load(file)
 
-# Define the key features
 features = {
     'anxiety_level': {'label': 'Anxiety Level', 'type': 'slider', 'min': 0, 'max': 10, 'value': 5},
     'self_esteem': {'label': 'Self Esteem', 'type': 'slider', 'min': 0, 'max': 10, 'value': 5},
@@ -20,13 +18,11 @@ features = {
     'social_support': {'label': 'Social Support', 'type': 'slider', 'min': 0, 'max': 10, 'value': 5},
 }
 
-# All required columns (including those not used in the form)
 all_columns = ['anxiety_level', 'self_esteem', 'mental_health_history', 'sleep_quality', 'academic_performance', 'social_support', 
                'peer_pressure', 'study_load', 'teacher_student_relationship', 'headache', 'future_career_concerns', 
                'living_conditions', 'depression', 'blood_pressure', 'safety', 'noise_level', 'bullying', 'basic_needs', 
                'extracurricular_activities', 'breathing_problem']
 
-# Default values for the columns not used in the form
 default_values = {
     'peer_pressure': 5,
     'study_load': 5,
@@ -44,7 +40,6 @@ default_values = {
     'breathing_problem': 0
 }
 
-# Create a form for user input
 st.title('Student Stress Level Prediction')
 
 st.markdown("""
@@ -59,23 +54,18 @@ for feature, config in features.items():
     elif config['type'] == 'selectbox':
         user_inputs[feature] = st.selectbox(config['label'], options=config['options'])
 
-# Convert categorical inputs to numerical
 user_inputs['mental_health_history'] = 1 if user_inputs['mental_health_history'] == 'Yes' else 0
 
-# Add default values for missing columns
 for column, default in default_values.items():
     if column not in user_inputs:
         user_inputs[column] = default
 
-# Ensure all required columns are present
 for column in all_columns:
     if column not in user_inputs:
         user_inputs[column] = 0
 
-# Convert user inputs into a DataFrame
 user_data = pd.DataFrame([user_inputs])
 
-# Preprocess the user inputs
 preprocessed_data = preprocessor.transform(user_data)
 
 # Make predictions
